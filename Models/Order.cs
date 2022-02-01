4989c8587c2843608df9e2d91b85b59e7f14b4a7
a-farhat <akfarhat@gmail.com>
2022-02-02 00:43:37 +0200

    Initial Commit

---
+++ Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaAppAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string State { get; set; }
+        public double Price { get; set; }
+        public string PaymentType { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Models/PizzaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PizzaAppAPI.Models
+{
+    public class PizzaContext : DbContext
+    {
+        public PizzaContext(DbContextOptions<PizzaContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; } = null!;
+    }
+}
